6f0741ba47b09ca3702656b91d86ef739204de49
ares97 <slowinskir@wp.pl>
2017-05-12 00:43:04 +0100

    enum used

---
+++ Organism.java
@@ -0,0 +1,105 @@
+import javafx.scene.paint.Color;
+
+import java.util.List;
+import java.util.Random;
+
+public abstract class Organism {
+    protected String announcements;
+    protected int age;
+    protected int strenght;
+    protected int initiation;
+    protected int posX, posY;
+    protected boolean toDelete;
+    public final Color color;
+    public final String name;
+    protected MyField fields[][];
+    protected Random generator;
+    protected List organisms;
+    protected int cooldown;
+
+    abstract void action();
+
+    void doTurn() {
+        if (toDelete)
+            organisms.remove(this);
+        announcements = "";
+        action();
+        age++;
+    }
+
+    abstract int reflectedAttack(Organism opponent);
+
+    public void emptyField() {
+        fields[posY][posX].setEmpty(true);
+        fields[posY][posX].setName("  ");
+        fields[posY][posX].setColor(Color.LIGHTGREEN);
+        fields[posY][posX].setOrganism(null);
+    }
+
+    Organism(MyField fields[][], int str, int init, Color color, String name, List<Organism> organisms) {
+        this.fields = fields;
+        announcements = "";
+        strenght = str;
+        initiation = init;
+        age = 0;
+        toDelete = false;
+        this.color = color;
+        this.name = name;
+        generator = new Random();
+        this.organisms = organisms;
+        if (setOrganismOnEmptyField()) {
+            fields[posY][posX].setOrganism(this);
+            fields[posY][posX].setName(name);
+            fields[posY][posX].setColor(color);
+        }
+    }
+
+    Organism(MyField fields[][], int str, int init, Color color, String name, List<Organism> organisms, int posX, int posY) {
+        this.fields = fields;
+        strenght = str;
+        initiation = init;
+        announcements = "";
+        age = 0;
+        toDelete = false;
+        this.color = color;
+        this.name = name;
+        generator = new Random();
+        this.organisms = organisms;
+        this.posX = posX;
+        this.posY = posY;
+        setOnField(posX, posY);
+    }
+
+    public void setOnField(int destX, int destY) {
+        posX = destX;
+        posY = destY;
+        fields[destY][destX].setColor(color);
+        fields[destY][destX].setName(name);
+        fields[destY][destX].setEmpty(false);
+        fields[destY][destX].setOrganism(this);
+    }
+
+    public boolean isFieldEmpty(int x, int y) {
+        return fields[y][x].isEmpty();
+    }
+
+    public boolean setOrganismOnEmptyField() {
+        Random generator = new Random();
+        int x = generator.nextInt(fields.length);
+        int y = generator.nextInt(fields[0].length);
+        for (int i = 0; i < fields[0].length; i++) {
+            for (int j = 0; j < fields.length; j++) {
+                if (isFieldEmpty((x + j) % fields.length, (y + i) % fields[0].length)) {
+                    posX = (x + j) % fields.length;
+                    posY = (y + i) % fields[0].length;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isInsideBoard(int x, int y) {
+        return x >= 0 && x < fields.length && y >= 0 && y < fields[0].length;
+    }
+}
--- Organisms.java
+++ Organisms.java
@@ -1,27 +1,26 @@
 import javafx.scene.paint.Color;
 
-public  enum Organisms {
-    ANTELOPE(4,4,"Antelope",Color.CORNSILK),
-    DANDELION(0,0,"Dandelion",Color.YELLOW),
-    FOX(3,7,"Fox", Color.DARKRED),
-    GRASS(0,0,"Grass",Color.GREEN),
-    GUARANA(0,0,"Guarana",Color.ORANGERED),
-    HUMAN(5,4,"Human",Color.TAN),
-    SHEEP(4,4,"Sheep",Color.WHITESMOKE),
-    SOSNOWSKY_HOGWEED(10,0,"Sosnowsky's Hogweed",Color.MEDIUMSEAGREEN),
-    TORTOISE(2,1,"Tortoise",Color.BROWN),
-    WILD_BERRY(99,0,"Wild Berry",Color.DARKVIOLET);
+public enum Organisms {
+    ANTELOPE(4, 4, "Antelope", Color.CORNSILK),
+    DANDELION(0, 0, "Dandelion", Color.YELLOW),
+    FOX(3, 7, "Fox", Color.DARKRED),
+    GRASS(0, 0, "Grass", Color.GREEN),
+    GUARANA(0, 0, "Guarana", Color.ORANGERED),
+    HUMAN(5, 4, "Human", Color.TAN),
+    SHEEP(4, 4, "Sheep", Color.WHITESMOKE),
+    SOSNOWSKY_HOGWEED(10, 0, "Sosnowsky's Hogweed", Color.MEDIUMSEAGREEN),
+    TORTOISE(2, 1, "Tortoise", Color.BROWN),
+    WILD_BERRY(99, 0, "Wild Berry", Color.DARKVIOLET),
+    WOLF(9, 5, "Wolf", Color.DARKGRAY);
 
-    final int strength,initiation;
+    final int strength, initiation;
     final String name;
     final Color color;
-    Organisms(int strength,int initiation,String name,Color color){
+
+    Organisms(int strength, int initiation, String name, Color color) {
         this.strength = strength;
         this.initiation = initiation;
         this.name = name;
         this.color = color;
     }
-
-
-
 }
