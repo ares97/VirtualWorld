c494123422e425398131703eb3f37339dd51eb8b
ares97 <slowinskir@wp.pl>
2017-05-11 23:43:18 +0100

    added game board and abstract class of organism

--- Main.java
+++ Main.java
@@ -1,37 +1,88 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.awt.event.ActionEvent;
 
 public class Main extends Application {
     private StartingWindow startingWindow;
-
+    private Stage firstStage;
+    private Parent root;
+    private int gameWidth;
+    private int gameHeight;
+    private Button nextTurn;
+    private Button saveGame;
+    private Group gameRoot;
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage firstStage) throws Exception {
+        this.firstStage = firstStage;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartingWindow.fxml"));
-        Parent root = fxmlLoader.load();
+        root = fxmlLoader.load();
         startingWindow = fxmlLoader.getController();
-        primaryStage.setTitle("Virtual World");
+        firstStage.setTitle("Virtual World");
         Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        firstStage.setScene(scene);
+        firstStage.show();
         handleStartingWindowUI();
     }
 
     private void handleStartingWindowUI() {
-        startingWindow.LoadButton.setOnAction(event -> {
-            //TODO do load recent game
-        });
-        startingWindow.startButton.setOnAction(event -> {
-            //TODO handle turning scene into gamefield
-        });
-
+        handleStartButton();
+        //todo handle load button
     }
-
+    private void handleStartButton(){
+        startingWindow.startButton.setOnAction(event -> {
+            createGameScene();
+        });
+    }
+    private void createGameScene(){
+        gameRoot = new Group();
+        getAndSaveSize();
+        addGameBoard();
+        addMenuBar();
+        firstStage.getScene().setRoot(gameRoot);
+        firstStage.sizeToScene();
+    }
+    private void addMenuBar(){
+        VBox vboxMenu = new VBox();
+        nextTurn = new Button("next turn");
+        saveGame = new Button("save");
+        ToolBar toolBar = new ToolBar(nextTurn,saveGame);
+        Label announcements = new Label("lorem loreml\nlorem loreml\nlorem loreml\nlorem loreml\nlorem loreml\n");
+        announcements.setWrapText(true);
+        announcements.setPrefHeight(gameHeight*MyField.rectangleHeight/2);
+        vboxMenu.setMinHeight(gameHeight*MyField.rectangleHeight);
+        vboxMenu.setMinWidth(MyField.rectangleWidth*9);
+        vboxMenu.setStyle("-fx-background-color: khaki; -fx-font-size: 13;");
+        vboxMenu.getChildren().addAll(toolBar,announcements);
+        gameRoot.getChildren().addAll(vboxMenu);
+        vboxMenu.setTranslateX(gameWidth*MyField.rectangleWidth);
+    }
+    private void addGameBoard(){
+        for (int i = 0; i < gameWidth; i++) {
+            for (int j = 0; j < gameHeight; j++) {
+                MyField node = new MyField(gameRoot, "", Color.LIGHTGREEN, i, j);
+                gameRoot.getChildren().add(node);
+                //fields[i][j] = node;
+            }
+        }
+    }
+    private void getAndSaveSize(){
+        try{
+            gameWidth = Integer.parseInt(startingWindow.sizeX.getText());
+            gameHeight = Integer.parseInt(startingWindow.sizeY.getText());
+        }catch(NumberFormatException exc){
+            Alert alert = new Alert(Alert.AlertType.ERROR );
+            alert.setContentText("KEY IN ONLY NUMBERS!");
+            alert.setTitle("Wrong size format!");
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
--- StartingWindow.java
+++ StartingWindow.java
@@ -1,9 +1,11 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 
 public class StartingWindow {
     public Button startButton;
     public Button LoadButton;
-
+    public TextField sizeX;
+    public TextField sizeY;
 }
